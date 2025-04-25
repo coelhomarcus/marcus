c7282929d133b63c6ff33e3a39eb2eac234770ee
Marcus Coelho <marcusrangelcoelho@gmail.com>
2025-04-25 01:12:06 -0300

    remove: accordion

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,11 +1,6 @@
 import { FaGithub, FaLinkedin } from "react-icons/fa";
-import { MdAlternateEmail } from "react-icons/md";
-import {
-    Accordion,
-    AccordionContent,
-    AccordionItem,
-    AccordionTrigger,
-} from "@/components/ui/accordion"
+import { IoDocumentText } from "react-icons/io5";
+
 
 import isMac from "@/utils/isMac";
 import ContactLink from "../ContactLink/ContactLink";
@@ -17,28 +12,19 @@ const About = ({ setOpen }: { setOpen: React.Dispatch<React.SetStateAction<boole
             <h1 className='text-xl font-semibold mb-4'>Marcus Coelho</h1>
             <p className='text-neutral-400 text-sm'>
                 Olá, me chamo Marcus Coelho, sou estudante de Sistemas de Informação na Unifesspa e estou sempre em busca de aprender coisas novas.
-                Se quiser saber mais sobre minha trajetória, <span><a className="transition-colors hover:text-neutral-200 underline" href="https://docs.google.com/document/d/1wgOhwh-1YT-LRog9j1tvxzBVKfraoSzps1AiBGuSx9A/export?format=pdf" download>veja meu currículo</a></span>.
+                Se quiser saber mais sobre minha trajetória, <span><a className="transition-colors text-neutral-200 hover:underline" href="https://docs.google.com/document/d/1wgOhwh-1YT-LRog9j1tvxzBVKfraoSzps1AiBGuSx9A/export?format=pdf" download>veja meu currículo</a></span>.
                 <br />
                 <br />
-                Atualmente, faço parte da Exception Jr, uma empresa júnior da faculdade, onde atuo como Full Stack Developer. Alguns dos <Link className="transition-colors hover:text-neutral-200 underline" to="/projects">projetos</Link> em que estou envolvido incluem o Conecta Canaã.
+                Gosto de criar sites bonitos, acessíveis e fáceis de usar. Trabalho no dia a dia com o ecossistema React, usando principalmente React, Vite/Next.js, Tailwind e TypeScript.
                 <br />
                 <br />
+                Atualmente, faço parte da Exception Jr, uma empresa júnior da faculdade, onde atuo como Full Stack Developer. Alguns dos <Link className="transition-colors text-neutral-200 hover:underline" to="/projects">projetos</Link> em que estou envolvido incluem o Conecta Canaã.
             </p>
-            <Accordion type="multiple">
-                <AccordionItem value="item-0">
-                    <AccordionTrigger className="text-neutral-200 cursor-pointer">Skills</AccordionTrigger>
-                    <AccordionContent className="space-y-2">
-                        <p><span className="text-neutral-200 text-xs font-bold">Front-end: </span>React, Typescript, Tailwind, Next.js, Vite.</p>
-                        <p><span className="text-neutral-200 text-xs font-bold">Back-end: </span>Go, Node.js, Express, PostgreSQL, MySQL. </p>
-                        <p><span className="text-neutral-200 text-xs font-bold">Outros: </span>Swift, Python, C# (Unity)</p>
-                    </AccordionContent>
-                </AccordionItem>
-            </Accordion>
             <div className="flex justify-between border-t mt-4 pt-4 border-neutral-800">
                 <div className="flex gap-3 text-white">
+                    <ContactLink title="CV" Icon={IoDocumentText} href="https://docs.google.com/document/d/1wgOhwh-1YT-LRog9j1tvxzBVKfraoSzps1AiBGuSx9A/export?format=pdf" download />
                     <ContactLink title="Github" Icon={FaGithub} href="https://github.com/coelhomarcus" />
                     <ContactLink title="Linkedin" Icon={FaLinkedin} href="https://www.linkedin.com/in/coelhomarcus/" />
-                    <ContactLink title="E-mail" Icon={MdAlternateEmail} href="mailto:marcusrangelcoelho@gmail.com" />
                 </div>
                 <button className="hidden sm:block cursor-pointer text-neutral-400 hover:text-neutral-500 transition-all duration-200" onClick={() => setOpen(true)}>
                     <div className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border px-1.5 font-mono text-xs font-medium  opacity-100">
@@ -46,7 +32,7 @@ const About = ({ setOpen }: { setOpen: React.Dispatch<React.SetStateAction<boole
                     </div>
                 </button>
             </div>
-        </div>
+        </div >
     )
 }
 
--- src/components/ContactLink/ContactLink.tsx
+++ src/components/ContactLink/ContactLink.tsx
@@ -1,10 +1,25 @@
-import { IconType } from "react-icons"
+import { IconType } from "react-icons";
+import { AnchorHTMLAttributes } from "react";
 
-const ContactLink = ({ Icon, title, href }: { Icon: IconType, title: string, href: string }) => {
-    return <a href={href} rel="noreferrer noopener" target='_blank' className="inline-flex items-center gap-1 font-medium text-xs transition-colors hover:text-neutral-400">
-        <Icon className="size-4" />
-        {title}
-    </a>
+interface ContactLinkProps extends AnchorHTMLAttributes<HTMLAnchorElement> {
+    Icon: IconType;
+    title: string;
+    href: string;
 }
+
+const ContactLink = ({ Icon, title, href, ...props }: ContactLinkProps) => {
+    return (
+        <a
+            href={href}
+            rel="noreferrer noopener"
+            target="_blank"
+            className="inline-flex items-center gap-1 font-medium text-xs transition-colors hover:text-neutral-400"
+            {...props}
+        >
+            <Icon className="size-4" />
+            {title}
+        </a>
+    );
+};
 
 export default ContactLink;
