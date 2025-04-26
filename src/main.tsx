203c81ebef8fe4f323af9f411d6bdb59b7fb2901
Marcus Coelho <marcusrangelcoelho@gmail.com>
2025-04-25 23:20:12 -0300

    fix: favicon issue

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,18 @@ import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router'
 
+const ensureFavicon = () => {
+  const exists = document.querySelector("link[rel='icon']");
+  if (!exists) {
+    const link = document.createElement('link');
+    link.rel = 'icon';
+    link.href = 'icons/favicon.ico';
+    document.head.appendChild(link);
+  }
+};
+
+ensureFavicon();
+
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <App />
