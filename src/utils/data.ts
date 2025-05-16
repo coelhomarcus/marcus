824be777ef7cee2ecaacb2ce665e21b86e9c650a
Marcus Coelho <marcusrangelcoelho@gmail.com>
2025-05-15 22:15:59 -0300

    new post: rest

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,180 +1,187 @@
 export const arrBlog = [
-  {
-    slug: "my-env",
-    title: "Meu Ambiente",
-    desc: "Ambiente, ferramentas, configurações e recomendações",
-    date: "26/04/2025",
-    time: 4,
-    tags: ["dotfiles", "recomendações"],
-  },
-  {
-    slug: "learning-express",
-    title: "Aprendendo Express",
-    desc: "Minhas anotações e estudos sobre Express.js",
-    date: "24/04/2025",
-    time: 7,
-    tags: ["express", "node"],
-  },
-  {
-    slug: "learning-go",
-    title: "Aprendendo GO",
-    desc: "Minhas anotações e estudos sobre Go (golang)",
-    date: "19/04/2025",
-    time: 18,
-    tags: ["go"],
-  },
+    {
+        slug: "rest",
+        title: "Descansar é importante",
+        desc: "Reflexões após um mês intenso de estudos e aprendizados",
+        date: "15/05/2025",
+        time: 3,
+        tags: ["experiência", "cotidiano", "recomendações"],
+    },
+    {
+        slug: "my-env",
+        title: "Meu Ambiente",
+        desc: "Ambiente, ferramentas, configurações e recomendações",
+        date: "26/04/2025",
+        time: 4,
+        tags: ["dotfiles", "recomendações"],
+    },
+    {
+        slug: "learning-express",
+        title: "Aprendendo Express",
+        desc: "Minhas anotações e estudos sobre Express.js",
+        date: "24/04/2025",
+        time: 7,
+        tags: ["express", "node"],
+    },
+    {
+        slug: "learning-go",
+        title: "Aprendendo GO",
+        desc: "Minhas anotações e estudos sobre Go (golang)",
+        date: "19/04/2025",
+        time: 18,
+        tags: ["go"],
+    },
 ];
 
 export const arrProjects = [
-  {
-    href: "https://cafuntalk.com",
-    name: "CafunTalk",
-    desc: "Bate-papo moderno, sem distrações, privado e sem histórico.",
-    tech: ["react", "ts", "tailwind", "express"],
-  },
-  {
-    href: "https://www.youtube.com/shorts/0fSoHjAadas",
-    name: "SOS Queimadas",
-    desc: "Aplicativo IOS para conscientização e prevenção de queimadas.",
-    tech: ["swift", "mapkit", "iot", "api"],
-  },
-  {
-    href: "https://smartcitycanaadoscarajas.com.br/index.php/conectacanaa",
-    name: "Conecta Canaã",
-    desc: "Plataforma de ocorrências e serviços públicos para Prefeitura de Canaã dos Carajás - PA.",
-    tech: ["react", "js", "mongodb"],
-  },
+    {
+        href: "https://cafuntalk.com",
+        name: "CafunTalk",
+        desc: "Bate-papo moderno, sem distrações, privado e sem histórico.",
+        tech: ["react", "ts", "tailwind", "express"],
+    },
+    {
+        href: "https://www.youtube.com/shorts/0fSoHjAadas",
+        name: "SOS Queimadas",
+        desc: "Aplicativo IOS para conscientização e prevenção de queimadas.",
+        tech: ["swift", "mapkit", "iot", "api"],
+    },
+    {
+        href: "https://smartcitycanaadoscarajas.com.br/index.php/conectacanaa",
+        name: "Conecta Canaã",
+        desc: "Plataforma de ocorrências e serviços públicos para Prefeitura de Canaã dos Carajás - PA.",
+        tech: ["react", "js", "mongodb"],
+    },
 ];
 
 export const arrSideProjects = [
-  {
-    href: "https://coelhomarcus.github.io/bunnybash/",
-    name: "BunnyBash",
-    desc: "Site que simula um terminal.",
-    tech: ["html", "css", "js", "api"],
-  },
-  {
-    href: "https://github.com/coelhomarcus/coebot",
-    name: "CoeBot",
-    desc: "Fun Bot para o Discord.",
-    tech: ["node", "discordjs", "js", "api"],
-  },
-  {
-    href: "https://marketplace.visualstudio.com/items?itemName=coelhomarcus.bakaneo",
-    name: "BakaNeo",
-    desc: "Tema dark para VS Code.",
-    tech: ["design"],
-  },
-  {
-    href: "https://blob-temp.vercel.app/",
-    name: "Blob",
-    desc: "Site de previsão meteorológica",
-    tech: ["next.js", "tailwind", "api"],
-  },
-  {
-    href: "https://coelhomarcus.itch.io/mailman",
-    name: "Mailman",
-    desc: "Protótipo de jogo de terror retrô.",
-    tech: ["c#", "unity", "blender"],
-  },
-  {
-    href: "https://coelhomarcus.github.io/catype/",
-    name: "Catype",
-    desc: "Protótipo de site de digitação.",
-    tech: ["html", "css", "js"],
-  },
+    {
+        href: "https://coelhomarcus.github.io/bunnybash/",
+        name: "BunnyBash",
+        desc: "Site que simula um terminal.",
+        tech: ["html", "css", "js", "api"],
+    },
+    {
+        href: "https://github.com/coelhomarcus/coebot",
+        name: "CoeBot",
+        desc: "Fun Bot para o Discord.",
+        tech: ["node", "discordjs", "js", "api"],
+    },
+    {
+        href: "https://marketplace.visualstudio.com/items?itemName=coelhomarcus.bakaneo",
+        name: "BakaNeo",
+        desc: "Tema dark para VS Code.",
+        tech: ["design"],
+    },
+    {
+        href: "https://blob-temp.vercel.app/",
+        name: "Blob",
+        desc: "Site de previsão meteorológica",
+        tech: ["next.js", "tailwind", "api"],
+    },
+    {
+        href: "https://coelhomarcus.itch.io/mailman",
+        name: "Mailman",
+        desc: "Protótipo de jogo de terror retrô.",
+        tech: ["c#", "unity", "blender"],
+    },
+    {
+        href: "https://coelhomarcus.github.io/catype/",
+        name: "Catype",
+        desc: "Protótipo de site de digitação.",
+        tech: ["html", "css", "js"],
+    },
 ];
 
 export const arrCertificates = [
-  {
-    href: "https://scrimba.com/certificate-cert2ffentAFMatUYR9SpXLrNVrLLKpQAeKvDPKMeAowGy3Ft1",
-    course: "Learn Express.js",
-    teacher: "Scrimba",
-  },
-  {
-    href: "https://scrimba.com/certificate-cert2ffentAFMatUYR9SpXLrNVhGP9VMRKwBNs5jXMeknpXvNn",
-    course: "Learn Next.js",
-    teacher: "Scrimba",
-  },
-  {
-    href: "https://static.frontendmasters.com/ud/c/4daf165d55/pwzNwRtJBS/complete-go.pdf",
-    course: "Complete Go for Professional Developers",
-    teacher: "Frontend Masters",
-  },
-  {
-    href: "https://www.ude.my/UC-f2b12b39-f172-4fcf-8dab-19fc29974950/",
-    course: "Go: The Complete Developer's Guide (Golang)",
-    teacher: "Stephen Grider",
-  },
-  {
-    href: "https://www.ude.my/UC-5d18ce0f-2c7f-45e0-b3d9-d40334d036ec/",
-    course: "The Complete Full-Stack Web Development Bootcamp",
-    teacher: "The App Brewery",
-  },
-  {
-    href: "https://scrimba.com/certificate-cert2ffentAFMatUYR9SpXLrNY6pYm2ty3x7mRPD7riJFvwkKP",
-    course: "Open-source AI Models",
-    teacher: "Scrimba",
-  },
-  {
-    href: "https://www.origamid.com/certificate/e768c9c3",
-    course: "React com TypeScript",
-    teacher: "Origamid",
-  },
-  {
-    href: "https://www.origamid.com/certificate/13f9b32b",
-    course: "React Completo",
-    teacher: "Origamid",
-  },
-  {
-    href: "https://www.origamid.com/certificate/84974e3e",
-    course: "TypeScript",
-    teacher: "Origamid",
-  },
-  {
-    href: "https://www.origamid.com/certificate/3fe3a556",
-    course: "Tailwind",
-    teacher: "Origamid",
-  },
-  {
-    href: "https://www.origamid.com/certificate/b1ee7546",
-    course: "JavaScript ES6+",
-    teacher: "Origamid",
-  },
-  {
-    href: "https://ticemtrilhas.org.br/certificate-validation?code=56fa7c5f-f904-40bd-829a-1bc41898df2b",
-    course: "Versionamento de Código",
-    teacher: "PUCRS",
-  },
-  {
-    href: "https://ticemtrilhas.org.br/certificate-validation?code=8e8ad089-6179-4b5b-acd7-ede483cdd348",
-    course: "Fundamentos de Desenvolvimento Web",
-    teacher: "TIC em Trilhas",
-  },
-  {
-    href: "https://ude.my/UC-b71eae4c-5ded-44ad-a562-9d3293fa8ec1/",
-    course: "Blender - Básico ao Avançado",
-    teacher: "Gustavo Rosa",
-  },
-  {
-    href: "https://ticemtrilhas.org.br/certificate-validation?code=731272b6-5951-4b31-928f-5ae202e4b56c",
-    course: "Introdução ao Desenvolvimento de Jogos - Unity",
-    teacher: "TIC em trilhas",
-  },
-  {
-    href: "https://ticemtrilhas.org.br/certificate-validation?code=29d9cad9-266c-4079-b076-98557f861e37",
-    course: "Modelagem 3D para Jogos - Blender",
-    teacher: "TIC em trilhas",
-  },
-  {
-    href: "https://lms.hackatruck.com.br/mod/simplecertificate/verify.php",
-    course:
-      "Práticas de Cloud Services usando Swift com ênfase em Serviços Cognitivos",
-    teacher: "Instituto de Pesquisas Eldorado (HACKATRUCK)",
-  },
-  {
-    href: "https://lms.hackatruck.com.br/mod/simplecertificate/verify.php",
-    course: "Algoritmos e Programação Orientada a Objetos com Swift",
-    teacher: "Instituto de Pesquisas Eldorado (HACKATRUCK)",
-  },
+    {
+        href: "https://scrimba.com/certificate-cert2ffentAFMatUYR9SpXLrNVrLLKpQAeKvDPKMeAowGy3Ft1",
+        course: "Learn Express.js",
+        teacher: "Scrimba",
+    },
+    {
+        href: "https://scrimba.com/certificate-cert2ffentAFMatUYR9SpXLrNVhGP9VMRKwBNs5jXMeknpXvNn",
+        course: "Learn Next.js",
+        teacher: "Scrimba",
+    },
+    {
+        href: "https://static.frontendmasters.com/ud/c/4daf165d55/pwzNwRtJBS/complete-go.pdf",
+        course: "Complete Go for Professional Developers",
+        teacher: "Frontend Masters",
+    },
+    {
+        href: "https://www.ude.my/UC-f2b12b39-f172-4fcf-8dab-19fc29974950/",
+        course: "Go: The Complete Developer's Guide (Golang)",
+        teacher: "Stephen Grider",
+    },
+    {
+        href: "https://www.ude.my/UC-5d18ce0f-2c7f-45e0-b3d9-d40334d036ec/",
+        course: "The Complete Full-Stack Web Development Bootcamp",
+        teacher: "The App Brewery",
+    },
+    {
+        href: "https://scrimba.com/certificate-cert2ffentAFMatUYR9SpXLrNY6pYm2ty3x7mRPD7riJFvwkKP",
+        course: "Open-source AI Models",
+        teacher: "Scrimba",
+    },
+    {
+        href: "https://www.origamid.com/certificate/e768c9c3",
+        course: "React com TypeScript",
+        teacher: "Origamid",
+    },
+    {
+        href: "https://www.origamid.com/certificate/13f9b32b",
+        course: "React Completo",
+        teacher: "Origamid",
+    },
+    {
+        href: "https://www.origamid.com/certificate/84974e3e",
+        course: "TypeScript",
+        teacher: "Origamid",
+    },
+    {
+        href: "https://www.origamid.com/certificate/3fe3a556",
+        course: "Tailwind",
+        teacher: "Origamid",
+    },
+    {
+        href: "https://www.origamid.com/certificate/b1ee7546",
+        course: "JavaScript ES6+",
+        teacher: "Origamid",
+    },
+    {
+        href: "https://ticemtrilhas.org.br/certificate-validation?code=56fa7c5f-f904-40bd-829a-1bc41898df2b",
+        course: "Versionamento de Código",
+        teacher: "PUCRS",
+    },
+    {
+        href: "https://ticemtrilhas.org.br/certificate-validation?code=8e8ad089-6179-4b5b-acd7-ede483cdd348",
+        course: "Fundamentos de Desenvolvimento Web",
+        teacher: "TIC em Trilhas",
+    },
+    {
+        href: "https://ude.my/UC-b71eae4c-5ded-44ad-a562-9d3293fa8ec1/",
+        course: "Blender - Básico ao Avançado",
+        teacher: "Gustavo Rosa",
+    },
+    {
+        href: "https://ticemtrilhas.org.br/certificate-validation?code=731272b6-5951-4b31-928f-5ae202e4b56c",
+        course: "Introdução ao Desenvolvimento de Jogos - Unity",
+        teacher: "TIC em trilhas",
+    },
+    {
+        href: "https://ticemtrilhas.org.br/certificate-validation?code=29d9cad9-266c-4079-b076-98557f861e37",
+        course: "Modelagem 3D para Jogos - Blender",
+        teacher: "TIC em trilhas",
+    },
+    {
+        href: "https://lms.hackatruck.com.br/mod/simplecertificate/verify.php",
+        course: "Práticas de Cloud Services usando Swift com ênfase em Serviços Cognitivos",
+        teacher: "Instituto de Pesquisas Eldorado (HACKATRUCK)",
+    },
+    {
+        href: "https://lms.hackatruck.com.br/mod/simplecertificate/verify.php",
+        course: "Algoritmos e Programação Orientada a Objetos com Swift",
+        teacher: "Instituto de Pesquisas Eldorado (HACKATRUCK)",
+    },
 ];
