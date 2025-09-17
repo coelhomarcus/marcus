ec5760339cd217ac85c6bf56d5666fb6879a09a6
Marcus Coelho <marcusrangelcoelho@gmail.com>
2025-09-17 00:23:40 -0300

    more text go

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -7,77 +7,77 @@ import { FaBriefcase } from "react-icons/fa";
 import { IoArrowForwardSharp } from "react-icons/io5";
 
 const About = () => {
-    return (
-        <main className="text-foreground">
-            <PageTitle title="Marcus Coelho" suffix="" />
-            <h1 className="text-xl font-semibold mb-2">Marcus Coelho</h1>
-            <p className="text-muted-foreground text-base mb-4">
-                Olá! Sou estudante de Sistemas de Informação (6/8) na Unifesspa, bolsista desenvolvedor no PET-Saúde:
-                Inovação e Saúde Digital no SUS, e também desenvolvedor na Exception Jr. Se quiser saber mais sobre
-                minha trajetória,{" "}
-                <span>
-                    <a
-                        className="transition-colors text-foreground hover:underline"
-                        href="https://docs.google.com/document/d/1wgOhwh-1YT-LRog9j1tvxzBVKfraoSzps1AiBGuSx9A/export?format=pdf"
-                        download
-                    >
-                        veja meu currículo{<CgExternal className="inline mb-1" />}
-                    </a>
-                </span>
-            </p>
-            <p className="text-muted-foreground text-base mb-4">
-                Atualmente utilizo o ecossistema <span className="text-foreground">React</span> para construir meus
-                projetos, meu conhecimento consistente em{" "}
-                <span className="text-foreground">Vite, Next.js, Tailwind, Typescript, Node.js</span> e pretendo
-                aprofundar meu conhecimento em <span className="text-foreground">Go</span>
-                <a href="https://coelhomarcus.com/blog/learning-go" target="_blank" rel="noopener noreferrer">
-                    <img src="./src/assets/gopher.svg" alt="Gopher" className="size-6 inline ml-1 active:scale-95" />
-                </a>
-                .
-            </p>
-            <div className="flex justify-center mb-4 gap-4">
-                <Link
-                    to={`/blog/${arrBlog[0].slug}`}
-                    className="group flex items-center justify-center gap-1 vw-fit text-xs text-muted-foreground border border-border hover:border-border/30 hover:bg-gradient-to-b hover:from-background hover:to-muted p-1 text-center transition-all duration-200"
-                >
-                    <div className="w-fit border border-border group-hover:border-transparent p-1 px-2 mr-1 text-center transition-all duration-400">
-                        <div className="relative inline-flex w-2 h-2 mr-1">
-                            <div className="absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75 animate-ping"></div>
-                            <div className="relative inline-flex w-2 h-2 bg-green-500 rounded-full"></div>
-                        </div>
-                        Novo
-                    </div>
-                    Post publicado! Vá conferir <IoArrowForwardSharp className="mr-1" />
-                </Link>
-            </div>
-            <WorksExperience />
-        </main>
-    );
+   return (
+      <main className="text-foreground">
+         <PageTitle title="Marcus Coelho" suffix="" />
+         <h1 className="text-xl font-semibold mb-2">Marcus Coelho</h1>
+         <p className="text-muted-foreground text-base mb-4">
+            Olá! Sou estudante de Sistemas de Informação (6/8) na Unifesspa, bolsista desenvolvedor no PET-Saúde:
+            Inovação e Saúde Digital no SUS, e também desenvolvedor na Exception Jr. Se quiser saber mais sobre
+            minha trajetória,{" "}
+            <span>
+               <a
+                  className="transition-colors text-foreground hover:underline"
+                  href="https://docs.google.com/document/d/1wgOhwh-1YT-LRog9j1tvxzBVKfraoSzps1AiBGuSx9A/export?format=pdf"
+                  download
+               >
+                  veja meu currículo{<CgExternal className="inline mb-1" />}
+               </a>
+            </span>
+         </p>
+         <p className="text-muted-foreground text-base mb-4">
+            Atualmente utilizo o ecossistema <span className="text-foreground">React</span> para construir meus
+            projetos, meu conhecimento consistente em{" "}
+            <span className="text-foreground">Vite, Next.js, Tailwind, Typescript, Node.js</span> e pretendo
+            aprofundar meu conhecimento em <span className="text-foreground">Go</span>
+            <a href="https://coelhomarcus.com/blog/learning-go" target="_blank" rel="noopener noreferrer">
+               <img src="./src/assets/gopher.svg" alt="Gopher" className="size-6 inline ml-1 active:scale-95" />
+            </a>
+            .
+         </p>
+         <div className="flex justify-center mb-4 gap-4">
+            <Link
+               to={`/blog/${arrBlog[0].slug}`}
+               className="group flex items-center justify-center gap-1 vw-fit text-xs text-muted-foreground border border-border hover:border-border/30 hover:bg-gradient-to-b hover:from-background hover:to-muted p-1 text-center transition-all duration-200"
+            >
+               <div className="w-fit border border-border group-hover:border-transparent p-1 px-2 mr-1 text-center transition-all duration-400">
+                  <div className="relative inline-flex w-2 h-2 mr-1">
+                     <div className="absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75 animate-ping"></div>
+                     <div className="relative inline-flex w-2 h-2 bg-green-500 rounded-full"></div>
+                  </div>
+                  Novo
+               </div>
+               Post publicado! Vá conferir <IoArrowForwardSharp className="mr-1" />
+            </Link>
+         </div>
+         <WorksExperience />
+      </main>
+   );
 };
 
 function WorksExperience() {
-    return (
-        <div className="space-y-6 gap-2">
-            <div className="flex items-center gap-2 text-foreground justify-between">
-                <p className="text-sm font-semibold">Experiência</p>
-                <FaBriefcase className="text-sm text-muted-foreground" />
+   return (
+      <div className="space-y-6 gap-2">
+         <div className="flex items-center gap-2 text-foreground justify-between">
+            <p className="text-sm font-semibold">Experiência</p>
+            <FaBriefcase className="text-sm text-muted-foreground" />
+         </div>
+         {arrWorks.map((work) => (
+            <div className="flex flex-col sm:flex-row gap-2 justify-between sm:items-center" key={work.company}>
+               <div className="flex gap-2 flex-col sm:flex-row">
+                  <img src={work.logo} alt={work.company} className="size-6 sm:size-10 object-cover rounded-sm" />
+                  <span>
+                     <h3>{work.company}</h3>
+                     <p className="text-[12px] sm:text-sm text-muted-foreground">{work.role}</p>
+                  </span>
+               </div>
+               <div>
+                  <p className="text-[12px] sm:text-sm text-muted-foreground">{work.duration}</p>
+               </div>
             </div>
-            {arrWorks.map((work) => (
-                <div className="flex flex-col sm:flex-row gap-2 justify-between sm:items-center" key={work.company}>
-                    <div className="flex gap-2 flex-col sm:flex-row">
-                        <img src={work.logo} alt={work.company} className="size-6 sm:size-10 object-cover rounded-lg" />
-                        <span>
-                            <h3>{work.company}</h3>
-                            <p className="text-[12px] sm:text-sm text-muted-foreground">{work.role}</p>
-                        </span>
-                    </div>
-                    <div>
-                        <p className="text-[12px] sm:text-sm text-muted-foreground">{work.duration}</p>
-                    </div>
-                </div>
-            ))}
-        </div>
-    );
+         ))}
+      </div>
+   );
 }
 
 export default About;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,49 +5,48 @@ import SidebarDrawer from "@/lib/ui/vaul";
 import { FaUserAlt } from "react-icons/fa";
 
 const Header = () => {
-    return (
-        <header className="flex justify-between items-center mb-4">
-            <NavLink to="/">
-                <Avatar className="size-10 sm:size-14 hover:scale-105 active:scale-95 transition-all duration-100 rounded-lg">
-                    <AvatarImage className="object-cover" src="https://github.com/coelhomarcus.png" />
-                    <AvatarFallback className="size-10 sm:size-15 rounded-lg">
-                        <FaUserAlt className="opacity-60 text-foreground" aria-hidden="true" />
-                    </AvatarFallback>
-                </Avatar>
-            </NavLink>
+   return (
+      <header className="flex justify-between items-center mb-4">
+         <NavLink to="/">
+            <Avatar className="size-10 sm:size-14 hover:scale-105 active:scale-95 transition-all duration-100 rounded-sm">
+               <AvatarImage className="object-cover" src="https://github.com/coelhomarcus.png" />
+               <AvatarFallback className="size-10 sm:size-15 rounded-sm">
+                  <FaUserAlt className="opacity-60 text-foreground" aria-hidden="true" />
+               </AvatarFallback>
+            </Avatar>
+         </NavLink>
 
-            <div className="flex items-center gap-4 sm:gap-5">
-                <div className="hidden md:flex gap-4 sm:gap-5 text-muted-foreground *:text-sm *:hover:text-foreground *:transition-colors">
-                    <HeaderLink title="Sobre" to="/" />
-                    <HeaderLink title="Projetos" to="/projects" />
-                    <HeaderLink title="Blog" to="/blog" />
-                </div>
-                <SidebarDrawer />
+         <div className="flex items-center gap-4 sm:gap-5">
+            <div className="hidden md:flex gap-4 sm:gap-5 text-muted-foreground *:text-sm *:hover:text-foreground *:transition-colors">
+               <HeaderLink title="Sobre" to="/" />
+               <HeaderLink title="Projetos" to="/projects" />
+               <HeaderLink title="Blog" to="/blog" />
             </div>
-        </header>
-    );
+            <SidebarDrawer />
+         </div>
+      </header>
+   );
 };
 
 function HeaderLink({ to, title }: { to: string; title: string }) {
-    return (
-        <NavLink
-            to={to}
-            className={({ isActive }) =>
-                `relative inline-block transition-colors ${isActive ? "text-foreground" : "text-muted-foreground"}`
-            }
-        >
-            {({ isActive }) => (
-                <span className="relative">
-                    {title}
-                    <span
-                        className={`absolute left-0 bottom-0 h-[1px] bg-foreground transition-all duration-500 ${
-                            isActive ? "w-full" : "w-0"
-                        }`}
-                    ></span>
-                </span>
-            )}
-        </NavLink>
-    );
+   return (
+      <NavLink
+         to={to}
+         className={({ isActive }) =>
+            `relative inline-block transition-colors ${isActive ? "text-foreground" : "text-muted-foreground"}`
+         }
+      >
+         {({ isActive }) => (
+            <span className="relative">
+               {title}
+               <span
+                  className={`absolute left-0 bottom-0 h-[1px] bg-foreground transition-all duration-500 ${isActive ? "w-full" : "w-0"
+                     }`}
+               ></span>
+            </span>
+         )}
+      </NavLink>
+   );
 }
 
 export default Header;
